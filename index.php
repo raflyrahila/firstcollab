e2693f3efc6faf75ac1c786bdbc1b16adba3c32a
raflyrahila <69660299+wreeckitralph@users.noreply.github.com>
2024-11-01 01:36:07 +0700

    menambahkan index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Simple PHP Program</title>
+</head>
+<body>
+    <h1>Welcome to My Simple PHP Program</h1>
+    <?php
+        echo "<p>Hello, World!</p>";
+        $currentDate = date("Y-m-d H:i:s");
+        echo "<p>Current Date and Time: $currentDate</p>";
+    ?>
+</body>
+</html>
